e1321749ab39bf46e7922ae5b09d0a6cbe69ff83
Greg Mercer (GBear) <gmercer@stanford.edu>
2016-05-20 14:42:54 -0700

    WP-7148 Added code to move the slick slide arrow buttons into a special arrow-wrapper

--- js/gsb_feature_page_ct_slideshow_slick.js
+++ js/gsb_feature_page_ct_slideshow_slick.js
@@ -11,6 +11,11 @@ Drupal.behaviors.gsb_feature_page_ct_slideshow_slick = {
       prevArrow: '<a class="flex-prev" href="#">Previous</a>',
       nextArrow: '<a class="flex-next" href="#">Next</a>',
     });
+    $('.slick-list').prepend('<div class="arrow-wrapper"></div>');
+    var prev_arrow = $('.flex-prev.slick-arrow').detach();
+    var next_arrow = $('.flex-next.slick-arrow').detach();
+    $('.arrow-wrapper').prepend(next_arrow);
+    $('.arrow-wrapper').prepend(prev_arrow);
   }
 };
 
